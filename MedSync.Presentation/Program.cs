426f44d4f3bcc54d815b965f6b3ccda38aaf1307
Jaloliddin | Jm7Uz <87977243+jm7uz@users.noreply.github.com>
2023-09-25 21:12:55 +0500

    Project layers are created.

---
+++ MedSync.Presentation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MedSync.Presentation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
